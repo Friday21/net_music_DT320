e685b1d227d3e7952e6eba24e4b4df7f70617b4a
zhuio <zhuiox@gmail.com>
2017-06-21 13:37:24 +0800

    修复bug

--- net_music_320_Tool.py
+++ net_music_320_Tool.py
@@ -1,8 +1,10 @@
 import requests
+import os
+
 print('请输入列表ID ')
 PLAY_LIST_ID = input()
 print('列表ID是 ',PLAY_LIST_ID)
-print('正在解析API')
+print('正在解析中......')
 PLAY_LIST_JSON = requests.get('https://api.imjad.cn/cloudmusic/?type=playlist&id=%s' % PLAY_LIST_ID).json()
 
 #获取歌曲名字
@@ -26,23 +28,37 @@ def get_songs_url():
         songs_url.append('https://api.imjad.cn/cloudmusic/?type=song&id=%s&br=320000' % id)
     return songs_url
 
-#下载歌曲
+#获取下载地址
+download_urls = []
+def get_download_urls():
+    for song_url in songs_url:
+        url = requests.get(song_url).json()['data'][0]['url']
+        download_urls.append(url)
+    return download_urls
+
+#生成字典
+download_dicts = {}
+def combine():
+    for i in range(len(song_name)):
+        download_dicts[song_name[i]] = download_urls[i]
+    return download_dicts
+
+#下载
 def download():
-    try:
-        for song_url in songs_url:
-            for name in song_name:
-                url = requests.get(song_url).json()['data'][0]['url']
-                r = requests.get(url)
-                print('开始下载%s到当前的文件夹' % name)
-                with open('%s.mp3' % name, 'wb') as f:
-                    f.write(r.content)
-                del song_name[0]
-                break
-        print('下载结束，看看去吧')
-    except Exception as e:
-        print('网易云版权问题，链接挂了')
-    finally:
-        download()
+    pwd = os.path.abspath('.')
+    directory = pwd+'/'+'music'
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    for key in download_dicts:
+        if download_dicts[key] == None:
+            print('这首%s有版权问题' % key)
+            continue
+        else:
+            r = requests.get(download_dicts[key])
+            print('开始下载%s到当前的文件夹' % key)
+            with open(directory+'/'+'%s.mp3' % key, 'wb') as f:
+                f.write(r.content)
+
 if __name__ == '__main__':
     try:
         get_song_id()
@@ -50,4 +66,6 @@ if __name__ == '__main__':
         get_song_name()
     except Exception as e:
         print('列表ID是否输对了啊？')
+    get_download_urls()
+    combine()
     download()
